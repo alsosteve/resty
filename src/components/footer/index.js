ca6849c7b753434f09174ee8bf38fa046b162f59
Steve Ngo <stevehngo@hotmail.com>
2022-02-20 22:18:49 -0500

    convert class to functions

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-class Footer extends React.Component {
-  render() {
-    return <footer>&copy; 2018</footer>;
-  }
+// class component
+
+// class Footer extends React.Component {
+//   render() {
+//     return <footer>&copy; 2018</footer>;
+//   }
+// }
+
+// functional component
+function Footer() {
+  return <footer>&copy; 2018</footer>;
 }
 
 export default Footer;
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <h1>RESTy</h1>
-      </header>
+// class component
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header>
+//         <h1>RESTy</h1>
+//       </header>
+//     );
+//   }
+// }
+
+
+// functional component
+function Header () {
+  return (
+          <header>
+            <h1>RESTy</h1>
+          </header>
     );
   }
-}
 
 export default Header;
